809e27c4e9e13f659d9f62c665ccab68b626a50b
Agnieszka000 <dluska.agnieszka@gmail.com>
2023-09-02 22:34:55 +0200

    problem set 1 ex3 added

---
+++ extensions.py
@@ -0,0 +1,25 @@
+# Implement a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends, 
+# case-insensitively, in any of these suffixes: .gif, .jpg, .jpeg, .png, .pdf, .txt, .zip.
+# If the file’s name ends with some other suffix or has no suffix at all, output application/octet-stream instead, which is a common default.
+
+def main():
+    ext = sep_endings()
+    check_ext(ext)
+
+def sep_endings():
+    file_end = filename.split(sep=".")
+    return file_end[-1]
+
+def check_ext(ext):
+    match ext:
+        case "gif" | "jpg" | "jpeg" | "png":
+            print (f"image/{ext}")
+        case "pdf" | "zip":
+            print(f"application/{ext}")
+        case "txt":
+            print("text/plain")
+        case _:
+            print("application/octet-stream")
+
+filename = input("File: ")
+main()
