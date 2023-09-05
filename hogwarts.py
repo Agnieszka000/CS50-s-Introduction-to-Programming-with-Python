1037fd47fcff0a1f59153d8f356d9b746fb18f27
Agnieszka000 <dluska.agnieszka@gmail.com>
2023-09-05 23:34:55 +0200

    hogwarts.py added

---
+++ hogwarts.py
@@ -0,0 +1,6 @@
+# The script prints a list of items from a list (students) and adds a number before every item.
+
+students = ["Hermione", "Harry", "Ron"]
+
+for i in range(len(students)): # Len finds the length of the list = for i in range(3)
+    print(i+1, students[i]) # [i] is the index of an item
